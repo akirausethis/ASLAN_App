2b42854836c3f99d1740b8279755d244dd4da4cc
Kenneth171204 <153161797+Kenneth171204@users.noreply.github.com>
2025-05-28 16:22:50 +0700

    Add files via upload

---
+++ AslanTests/AslanTests.swift
@@ -0,0 +1,17 @@
+//
+//  AslanTests.swift
+//  AslanTests
+//
+//  Created by Student on 21/05/25.
+//
+
+import Testing
+@testable import Aslan
+
+struct AslanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
